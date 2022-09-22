31d9b83b0657a55e1d7b01b6cab5b606384964e2
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-22 14:40:07 +0000

    Update files

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 
 
-// let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
+let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
 
-// function numberGen() {
-//     let result = '';
-//     for (let i = 0; i < 99; i++) {
-//         result += ((i+1) + '-' + symbols[i % symbols.length] + '<br>');
-//     }
-//     return result
-// }
+function generateSymbols() {
+    let result = '';
+    for (let i = 0; i < 99; i++) {
+        result += ((i+1) + '-' + symbols[i % symbols.length] + '<br>');
+    }
+    return result
+}
+
+
 
 // console.log(numberGen.result);
-let topText = document.getElementById('topText');
-let helpText = document.getElementById('helpText');
-let nextButton = document.getElementById('nextButton');
-let goResetButton = document.getElementById('goResetButton');
+// let topText = document.getElementById('topText');
+// let helpText = document.getElementById('helpText');
+// let nextButton = document.getElementById('nextButton');
+// let goResetButton = document.getElementById('goResetButton');
 
 function cardOne() {
     document.getElementById("topText").innerHTML = "I can read your mind";
@@ -45,6 +47,42 @@ function cardThree() {
     document.getElementById('nextButton').onclick = function(){cardFour()}
     document.getElementById('helpText').style.visibility = 'visible';
     document.getElementById('helpText').innerHTML = 'Ex: 14 is 1 + 4 + 5 click next to proceed';
+    document.getElementById('goButton').style.visibility = 'hidden';
+    document.getElementById('goResetButton').style.visibility  = 'visible';
+    document.getElementById('goResetButton').onclick = function(){cardOne()};
+}
+
+function cardFour() {
+    document.getElementById('topText').innerHTML = 'Subtract your new number from the original number';
+    document.getElementById('nextButton').style.visibility = 'visible';
+    document.getElementById('nextButton').innerHTML = 'NEXT';
+    document.getElementById('nextButton').onclick = function(){cardFive()}
+    document.getElementById('helpText').style.visibility = 'visible';
+    document.getElementById('helpText').innerHTML = 'Ex: 14 - 5 = 9 click next to proceed';
+    document.getElementById('goButton').style.visibility = 'hidden';
+    document.getElementById('goResetButton').style.visibility  = 'visible';
+    document.getElementById('goResetButton').onclick = function(){cardOne()};
+}
+
+function cardFive() {
+    document.getElementById('topText').innerHTML = 'Symbol shuffle funtion goes here';
+    document.getElementById('nextButton').style.visibility = 'visible';
+    document.getElementById('nextButton').innerHTML = 'REVEAL';
+    document.getElementById('nextButton').onclick = function(){cardSix()}
+    document.getElementById('helpText').style.visibility = 'visible';
+    document.getElementById('helpText').innerHTML = 'Find your new number. Match the symbol beside the number';
+    document.getElementById('goButton').style.visibility = 'hidden';
+    document.getElementById('goResetButton').style.visibility  = 'visible';
+    document.getElementById('goResetButton').onclick = function(){cardOne()};
+}
+
+function cardSix() {
+    document.getElementById('topText').innerHTML = 'Returned symbol goes here';
+    document.getElementById('nextButton').style.visibility = 'hidden';
+    document.getElementById('nextButton').innerHTML = 'REVEAL';
+    // document.getElementById('nextButton').onclick = function(){cardSix()}
+    document.getElementById('helpText').style.visibility = 'visible';
+    document.getElementById('helpText').innerHTML = 'Your symbol is:' + "returned symbol";
     document.getElementById('goButton').style.visibility = 'hidden';
     document.getElementById('goResetButton').style.visibility  = 'visible';
     document.getElementById('goResetButton').onclick = function(){cardOne()};
